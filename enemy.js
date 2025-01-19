a3ae81484d73e9a2711065edb0ebbe40732193ac
Dirk de Boer <stofkat@gmail.com>
2025-01-19 18:37:43 +0100

    working on enemy movement logic

--- enemy.js
+++ enemy.js
@@ -1,10 +1,11 @@
+import { Block } from "./block.js";
 import { GameObject } from "./gameObject.js";
 
 export class Enemy extends GameObject {
-  constructor(x, y,) {
+  constructor(x, y) {
     super(x, y, 60, 64);
     this.speed = 1;
-    this.dx = this.speed;
+    this.dx = Math.random() < 0.5 ? -this.speed : this.speed; // Randomly move left or right
     this.dy = 0;
     this.gravity = 0.5;
     this.grounded = true;
@@ -12,10 +13,10 @@ export class Enemy extends GameObject {
     this.tickCount = 0;
     this.ticksPerFrame = 3;
     this.numberOfFrames = 8;
-    this.facingRight = true;
+    this.facingRight = this.dx > 0;
 
     this.image = new Image();
-    this.image.src = './assets/gnome.png';
+    this.image.src = "./assets/gnome.png";
     this.image.onload = () => {
       this.loaded = true;
     };
@@ -44,16 +45,38 @@ export class Enemy extends GameObject {
     }
   }
 
-  update(blocks, scrollOffset) {
-
+  update(objects, scrollOffset) {
+    this.x += this.dx;
     this.tickCount += 1;
     if (this.tickCount > this.ticksPerFrame) {
       this.tickCount = 0;
       this.frameIndex = (this.frameIndex + 1) % this.numberOfFrames;
     }
 
-    if (this.grounded) {
-      this.x += this.dx;
+    // Check for block diagonally underneath in the direction they are facing
+    let onBlock = false;
+    const checkX = (this.facingRight ? this.x + (this.width /2) : this.x - (this.width/2)) -scrollOffset;
+    const checkY = this.y  + (this.height /2);
+
+    console.log("checkX", checkX);
+    console.log("checkY", checkY);
+
+    objects.forEach((gameObj) => {
+      if (
+        gameObj instanceof Block,
+        checkX < gameObj.x + gameObj.width  &&
+        checkX + this.width > gameObj.x &&
+        checkY < gameObj.y + gameObj.height &&
+        checkY + this.height > gameObj.y
+      ) {
+        onBlock = true;
+        console.log("on block");
+      }
+    });
+
+    if (!onBlock) {
+      this.dx = -this.dx; // Turn in the opposite direction
+      this.facingRight = this.dx > 0;
     }
   }
 }
